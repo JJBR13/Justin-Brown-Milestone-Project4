239160e66f2e87ac53c5715b8324c0f9b81d61b9
Justin Brown <103211737+JJBR13@users.noreply.github.com>
2023-04-17 16:42:35 +0100

    Customise admin

--- tour_products/admin.py
+++ tour_products/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import TourProducts
 
+class TourProductsAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'description',
+        'price',
+        'four_facts',
+        'date_available',
+        'image', 
+    )
+
 admin.site.register(TourProducts)
 
 
--- tour_products/models.py
+++ tour_products/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 class TourProducts(models.Model):
+
+    class Meta: 
+        verbose_name_plural = 'Tour Product'
+
     name = models.CharField(max_length=60)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
