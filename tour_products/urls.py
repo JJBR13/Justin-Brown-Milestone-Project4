30fa9437dd974e9686f6d41be6b9358d304234e7
Justin Brown <103211737+JJBR13@users.noreply.github.com>
2023-04-18 13:50:50 +0100

    Tour breakdown setup

--- tour_products/urls.py
+++ tour_products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_tours, name='all-tours')
+    path('', views.all_tours, name='all-tours'),
+    path ('<tour_id>', views.tour_breakdown, name="tour_breakdown"),
 ]
--- tour_products/views.py
+++ tour_products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import TourProducts
 
 
@@ -12,3 +12,14 @@ def all_tours(request):
     }
 
     return render(request, 'all_tours/tour_products.html', context)
+
+def tour_details(request, tour_id):
+    ''' View that shows individual tours '''
+
+    tour = get_object_or_404(TourProducts, pk=tour_id)
+
+    context = {
+        'tourproducts': tourproducts,
+    }
+
+    return render(request, 'all_tours/tour_breakdown.html', context)
