0eb0637d366f861d379411d6c96439573a427cdc
Justin Brown <103211737+JJBR13@users.noreply.github.com>
2023-04-18 17:27:39 +0100

    Add search bar query to views.py

--- tour_products/views.py
+++ tour_products/views.py
@@ -1,14 +1,29 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import TourProducts
 
 
 def all_tours(request):
-    ''' A view that shows all tours '''
+    ''' A view that shows all tours & Allows search bar function'''
 
     tourproducts = TourProducts.objects.all()
+    query = None
+
+    # Search bar query 
+    if request.GET:
+        if 's-q' in request.GET:
+            query = request.GET['sq']
+            if not query:
+                messages.error(request, "Invalid search criteria")
+                return redirect(reverse('all-tours'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            tourproducts = tourproducts.filter(queries)
 
     context = {
         'tourproducts': tourproducts,
+        'search_text': query,
     }
 
     return render(request, 'tours/tour_products.html', context)
