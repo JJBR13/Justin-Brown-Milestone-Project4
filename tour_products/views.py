00a5cd3b7354f8166005e668e9ddffa9366b4834
Justin Brown <103211737+JJBR13@users.noreply.github.com>
2023-04-18 07:39:00 +0100

    Add all tours views and templates

--- tour_products/views.py
+++ tour_products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import TourProducts
 
-# Create your views here.
+
+def all_tours(request):
+    ''' A view that shows all tours '''
+
+    tourproducts = TourProducts.objects.all()
+
+    context = {
+        'tourproducts': tourproducts,
+    }
+
+    return render(request, 'tourproducts/tour_products.html', context)
