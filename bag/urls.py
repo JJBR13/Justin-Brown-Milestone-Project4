a4ad51a14199960cdee037ec0b047bdeabbdea80
Justin Brown <103211737+JJBR13@users.noreply.github.com>
2023-04-20 16:19:27 +0100

    Add add_to_backpack function

--- bag/urls.py
+++ bag/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_bag, name='view-bag')
+    path('', views.view_bag, name='view-bag'),
+    path('add/<item_id>', views.add_to_backpack, name='add-to-backpack'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,3 +7,25 @@ def view_bag(request):
     """ Returns view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_backpack(request, item_id):
+    """ Add tour to backpack """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
+    # Get varible if already extists if not creates one
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        # update quantity if exsists
+        bag[item_id] += quantity
+    else:
+        # Add to bag
+        bag[item_id] = quantity
+
+    # Update/create bag on session 
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
