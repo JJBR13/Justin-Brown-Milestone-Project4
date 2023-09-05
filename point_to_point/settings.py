5edb51f17fea5c22adc4f48effd730357549223b
JJBR13 <jjbrown1308@gmail.com>
2023-09-05 14:44:52 +0000

    Add access to allowed_hots, setting.py

--- point_to_point/settings.py
+++ point_to_point/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-@3$f$wotqq++i&$22k_)o88#c5%=7_ukoos*+zqm33ptk5t=d(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['8000-jjbr13-justinbrownmiles-9f7wwte36c2.ws-eu104.gitpod.io']
 
 # Application definition
 
