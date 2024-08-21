a61fe8b8b2821bf85ae9edaaef0414e3d709500d
JJBR13 <jjbrown1308@gmail.com>
2024-08-21 10:50:32 +0000

    Js added to prevent next btn if required field isd not complete

--- checkout/static/checkout/js/next_step.js
+++ checkout/static/checkout/js/next_step.js
@@ -1,9 +1,38 @@
 function nextStep(step) {
-    document.querySelectorAll('.checkout-step').forEach(function (element) {
+
+    var currentStep = document.getElementById('step-' + (step - 1));
+    
+    if (!validateStep(currentStep)) {
+        return false;  // Prevent moving to the next step if validation fails
+    }
+    
+    // Hides all steps
+    document.querySelectorAll('.checkout-step').forEach(function(element) {
         element.style.display = 'none';
     });
+    
+    // Show the next step
     document.getElementById('step-' + step).style.display = 'block';
+    
     updateProgressBar(step);
+}
+
+function validateStep(stepElement) {
+    var isValid = true;
+    
+    // Find all required fields within the current step
+    var requiredFields = stepElement.querySelectorAll('[required]');
+    
+    requiredFields.forEach(function(field) {
+        if (!field.value) {
+            field.classList.add('is-invalid');  // Highlight the field with an error
+            isValid = false;
+        } else {
+            field.classList.remove('is-invalid');
+        }
+    });
+    
+    return isValid;
 }
 
 function prevStep(step) {
