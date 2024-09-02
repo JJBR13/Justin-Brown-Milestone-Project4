94faa820230fd6e54c15504941ac248f0888cc81
JJBR13 <jjbrown1308@gmail.com>
2024-09-02 10:16:26 +0000

    stripe_elements.js restructured for style & input

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -3,21 +3,31 @@ var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
 
+
+// Create separate elements for card number, expiry, and CVC
 var style = {
     base: {
-        color: '#000',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        color: '#005792',
+        fontFamily: '"Fredoka One", cursive',
         fontSmoothing: 'antialiased',
         fontSize: '16px',
+        backgroundColor: '#FFE6EB',
         '::placeholder': {
             color: '#aab7c4'
-        }
+        },
+        iconColor: '#005792'
     },
     invalid: {
         color: '#dc3545',
         iconColor: '#dc3545'
     }
 };
-var card = elements.create('card', {style: style});
-card.mount('#card-element');
+// Create separate elements for card number, expiry, and CVC
+var cardNumber = elements.create('cardNumber', { style: style });
+var cardExpiry = elements.create('cardExpiry', { style: style });
+var cardCvc = elements.create('cardCvc', { style: style });
 
+// Mount the elements to the respective divs
+cardNumber.mount('#card-number-element');
+cardExpiry.mount('#card-expiry-element');
+cardCvc.mount('#card-cvc-element');
