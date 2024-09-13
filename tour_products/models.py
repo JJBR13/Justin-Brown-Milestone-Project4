bb4b7e94ee3028c7f1986598b745f04e9c8a80c6
JJBR13 <jjbrown1308@gmail.com>
2024-09-13 14:05:26 +0000

    Created product admin form

--- tour_products/models.py
+++ tour_products/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
 
 class TourProducts(models.Model):
 
     class Meta:
         verbose_name_plural = 'Tour Product'
 
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=60)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
