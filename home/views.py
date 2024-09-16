ce010925f1c0d1f112ecd2510f11d5b5dec7fdea
JJBR13 <jjbrown1308@gmail.com>
2024-09-16 06:45:58 +0000

    Styling and page layout changes to index.html

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from tour_products.models import TourProducts
 
 
 def index(request):
     ''' Return homepage '''
+
+    tourproducts = TourProducts.objects.all()
+    
+    context = {
+        'tourproducts': tourproducts
+    }
 
     return render(request, 'home/index.html')
