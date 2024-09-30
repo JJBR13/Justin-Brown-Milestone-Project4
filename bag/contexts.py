de4eb5e2fa24fef5bf5d6623191e2d675721e337
JJBR13 <jjbrown1308@gmail.com>
2024-09-30 11:53:54 +0000

    Code refactored on bag  app

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.shortcuts import get_object_or_404
 from tour_products.models import TourProducts
 
--- bag/urls.py
+++ bag/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
-from .views import update_backpack
 
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<item_id>', views.add_to_backpack, name='add-to-backpack'),
     path('update/<item_id>/', views.update_backpack, name='update_backpack'),
-    
+
     path('remove/<int:item_id>/', views.remove_from_backpack, name='remove_from_backpack'),
 ]
